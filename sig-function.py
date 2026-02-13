bb5f94465657a18df8fe43c40549016e87e6072f
henriquefae <henriquegfae@gmail.com>
2026-02-13 15:23:25 +0100

    ;

--- sig-function.py
+++ sig-function.py
@@ -1,8 +1,9 @@
 import numpy as np
+import History
 
-def sig(p: float,h,e:float, n: int) -> str:
+def sig(p: float,h: History,e:float, n: int) -> str:
     bound = p*h.m+e*max(np.ln(n), np.sqrt(np.ln(n)*p*h.m))
-    
+
     if (p == 1 / n or p == 1 / 2) and h.ones >= bound:
         return "up"
     elif (p == 1 / n or p == 1 / 2) and h.zeros >= bound:
