4b17f836e942c3cceabfe3674d2a007133809386
Matheus Franco Siqueira <math.franco2002@gmail.com>
2026-02-15 14:12:22 +0100

    test updated

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
-from Individual import Individual
-from FrequencyVector import FrequencyVector
 from OriginalHistory import OriginalHistory
 
-h = OriginalHistory(10)
-for i in range(33):
-    h.add(1)
+class Test:
+    def test_history_consolidation():
+        h = OriginalHistory(10)
+        for i in range(33):
+            h.add(1)
 
-curr = h.head
-while curr is not None:
-    print(f"m: {curr.m}, ones: {curr.ones}, zeros: {curr.zeros}")
-    curr = curr.next
+        curr = h.head
+        while curr is not None:
+            print(f"m: {curr.m}, ones: {curr.ones}, zeros: {curr.zeros}")
+            curr = curr.next
+
+    def test_benchmark_functions():
+        pass
+
+Test.test_history_consolidation()
